22a29b22231686040d6d0bf417cfff50a192151b
G-acharya12 <144044310+G-acharya12@users.noreply.github.com>
2023-09-05 01:20:42 +0530

    Update Time.java

--- day_1_assignment/Time.java
+++ day_1_assignment/Time.java
@@ -5,4 +5,11 @@ public class Days
 	public static void main(String[] args)
 	{
 	Scanner sc=new Scanner(System.in);
+		int milisecond=sc.nextInt();
+		int h=milisecond/3600000;
+		milisecond=milisecond-3600000*h;
+		int mint=milisecond/60000;
+		milisecond=milisecond-60000*mint;
+		int sec=milisecond/1000;
+		System.out.println("time"+h+mint+sec);
 }
